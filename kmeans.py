7e77a3d24af5a9f5115e06e2b47ad817152199f6
Thomas Roulin <thomasroulinCH@gmail.com>
2016-05-04 12:32:59 +0200

    You are a bit cheeky, aren't you ?

---
+++ kmeans.py
@@ -0,0 +1,16 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+from sklearn.cluster import KMeans
+from sklearn.datasets import make_blobs
+
+if __name__ == '__main__':
+    elems = list()
+    elems.append((2, 10))
+    elems.append((2, 5))
+    elems.append((8, 4))
+    elems.append((5, 8))
+    elems.append((7, 5))
+    elems.append((6, 4))
+    elems.append((1, 2))
+    elems.append((4, 9))
