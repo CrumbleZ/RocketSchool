fbc9e9df23a61fd4ab1b31f30d9541e924f43cf5
Thomas Roulin <thomasroulinCH@gmail.com>
2016-05-06 15:07:19 +0200

    work on Matching ranks. Not working yet, but on the way. rafojr10jp

--- match_ranks.py
+++ match_ranks.py
@@ -3,57 +3,64 @@ import numpy as np
 from copy import copy
 from classes import *
 
-# CONSTS
+
+# ==================================================#
+#                     UTILS                        #
+# ==================================================#
+def remove_same(coords):
+    coords.sort()
+    i = 1
+    while i < len(coords):
+        if coords[i] - coords[i - 1] < 64:
+            coords.pop(i)
+        else:
+            i += 1
+
+
+# ==================================================#
+#                     CONSTANTS                    #
+# ==================================================#
 SB_WIDTH = 1500
 SB_HEIGHT = 700
 
 
-# CLASSES
-class RocketUtils:
-    SKILL_TIER_COUNT = 16
-    SKILL_TIER_PATH = 'resources/SkillTierIcons/Icon_SkillGroup{}.png'
+# ==================================================#
+#                     CORE                         #
+# ==================================================#
+def main(scoreboard_path):
+    sb_path = scoreboard_path
+    sb_img_raw = cv2.imread(sb_path)
+    sb_img = cv2.resize(sb_img_raw, (SB_WIDTH, SB_HEIGHT), interpolation=cv2.INTER_CUBIC)[0:700, 0:200]
+    # sb_img = copy(sb_img_raw)
 
-    @staticmethod
-    def templates_path():
-        return [RocketUtils.SKILL_TIER_PATH.format(i) for i in range(RocketUtils.SKILL_TIER_COUNT)]
+    img_b, img_g, img_r = cv2.split(sb_img)
 
+    channels = [img_b, img_g, img_r]
+    imgs = [copy(sb_img) for i in range(3)]
 
-# MAIN
-sb_path = 'scoreboards/scoreboard0.png'
-sb_img_raw = cv2.imread(sb_path)
-sb_img = cv2.resize(sb_img_raw, (SB_WIDTH, SB_HEIGHT), interpolation=cv2.INTER_CUBIC)[0:700, 0:200]
-# sb_img = copy(sb_img_raw)
+    py = {}
+    T_S = 64
+    for r in ranks:
+        path = 'resources/SkillTierIcons/Icon_SkillGroup{}.png'.format(r.level)
+        template = cv2.imread(path, 0)
+        w, h = template.shape[::-1]
+        for i in range(len(channels)):
+            res = cv2.matchTemplate(channels[i], template, cv2.TM_CCOEFF_NORMED)
+            threshold = 0.7
+            loc = np.where(res >= threshold)
+            py += {pt[1]: r for pt in zip(*loc[::-1])}
 
-img_b, img_g, img_r = cv2.split(sb_img)
+    remove_same(py)
 
-channels = [img_b, img_g, img_r]
-imgs = [copy(sb_img) for i in range(3)]
+    for pt in py:
+        cv2.rectangle(imgs[0], (140, pt), (224, pt + 64), (0, 0, 255), 2)
 
-py = []
-T_S = 0
-for r in ranks:
-    path = 'resources/SkillTierIcons/Icon_SkillGroup{}.png'.format(r.level)
-    template = cv2.imread(path, 0)
-    w, h = template.shape[::-1]
-    T_S = w
-    for i in range(len(channels)):
-        res = cv2.matchTemplate(channels[i], template, cv2.TM_CCOEFF_NORMED)
-        threshold = 0.7
-        loc = np.where(res >= threshold)
-        py += [pt[1] for pt in zip(*loc[::-1])]
+    print(py)
 
-        #   cv2.rectangle(imgs[i], pt, (pt[0] + w, pt[1] + h), (0, 0, 255), 2)
-
-cool = []
-cool += py[0]
-py.sort()
-for i in range(len(py) - 1):
-    if py[i + 1] - py[i] > T_S:
-        cool += py[i+1]
+    for i in range(len(imgs)):
+        cv2.imshow("{}".format(i), imgs[i])
+    cv2.waitKey(0)
 
 
-print(py)
-
-for i in range(len(imgs)):
-    cv2.imshow("{}".format(i), imgs[i])
-cv2.waitKey(0)
+if __name__ == '__main__':
+    main('scoreboards/scoreboard0.png')
