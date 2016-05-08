5e44162f4f548f19feac8fbb69ac6927ea84965e
Gabriel Freitas <frg.gabriel.freitas@gmail.com>
2016-05-08 16:57:34 +0200

    Extracted players score

--- knearest_test.py
+++ knearest_test.py
@@ -21,23 +21,23 @@ model.train(samples, cv2.ml.ROW_SAMPLE, responses)
 im = cv2.imread("./resources/tmp.png")
 out = np.zeros(im.shape, np.uint8)
 gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
-blur = cv2.GaussianBlur(gray, (5, 5), 0)	#added
+blur = cv2.GaussianBlur(gray, (5, 5), 0)  # added
 thresh = cv2.adaptiveThreshold(blur, 255, 1, 1, 11, 2)
 
 im2, contours, hierarchy = cv2.findContours(thresh, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
 
 for cnt in contours:
-	if cv2.contourArea(cnt) > 100:
-		[x, y, w, h] = cv2.boundingRect(cnt)
-		if 20 < h < 25 and w < 20:
-			cv2.rectangle(im, (x, y), (x + w, y + h), (0, 255, 0), 2)
-			roi = thresh[y:y + h, x:x + w]
-			roismall = cv2.resize(roi, (10, 10))
-			roismall = roismall.reshape((1, 100))
-			roismall = np.float32(roismall)
-			retval, results, neigh_resp, dists = model.findNearest(roismall, k=1)
-			string = str(int((results[0][0])))
-			cv2.putText(out, string, (x, y + h), 0, 1, (0, 255, 0))
+    if cv2.contourArea(cnt) > 100:
+        [x, y, w, h] = cv2.boundingRect(cnt)
+        if 20 < h < 25 and w < 20:
+            cv2.rectangle(im, (x, y), (x + w, y + h), (0, 255, 0), 2)
+            roi = thresh[y:y + h, x:x + w]
+            roismall = cv2.resize(roi, (10, 10))
+            roismall = roismall.reshape((1, 100))
+            roismall = np.float32(roismall)
+            retval, results, neigh_resp, dists = model.findNearest(roismall, k=1)
+            string = str(int((results[0][0])))
+            cv2.putText(out, string, (x, y + h), 0, 1, (0, 255, 0))
 
 cv2.imshow('im', im)
 cv2.imshow('out', out)
